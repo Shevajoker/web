e7bcb9509a83a3f91c6dc8843bc9c38ef7bfa35b
Shevajoker <Shevajoker@gmail.com>
2016-02-03 22:59:35 +0300

    Вход через mysql

--- php/checkLogin.php
+++ php/checkLogin.php
@@ -1,14 +1,32 @@
 <?php
 // header ('Location: ../html/emter.html');
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+
 $link = mysql_connect('localhost', 'root');
-if (!$link) {
-    die('Ошибка соединения: ' . mysql_error());
-    echo 'true';
+ if (!$link) {
+     die('Ошибка соединения: ' . mysql_error());
+ }
+ mysql_select_db('anrex') or die('Не удалось выбрать базу данных');
+  $query = 'SELECT * FROM users WHERE name_users = "'.$user.'"';
+  $result = mysql_query($query) or die('Запрос не удался: ' . mysql_error());
+$row = mysql_fetch_array($result);
 
+$user_sql = $row['name_users'];
+$pass_sql = $row['pass_users'];
+
+if($user == $user_sql && $pass == $pass_sql)
+{
+  echo 'true';
 }
-echo 'false';
+else {
+  echo 'false';
+}
 
-mysql_close($link);
+
+ // Освобождаем память от результата
+ mysql_free_result($result);
+ mysql_close($link);
 
 
  ?>
